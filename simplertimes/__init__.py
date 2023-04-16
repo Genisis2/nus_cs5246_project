b996901da04054909390d9af45fdf275f05a9e75
Hans Golong <hans.golong@gmail.com>
2023-04-16 14:48:07 +0800

    Pushing run of vocabulary generation

--- simplertimes/__init__.py
+++ simplertimes/__init__.py
@@ -1,15 +1,1 @@
 from . import data, simplify, summarize, vocab
-
-def simplify_news():
-    """Simplifies the news.
-    
-    """
-
-    # Step 1: Generate abstractive summary
-
-    # Step 2: Simplify generated summary
-
-    # Step 3: Generate assistive vocabulary list
-    vocab.generate_vlist()
-
-    pass
